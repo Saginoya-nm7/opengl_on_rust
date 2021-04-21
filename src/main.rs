1e6e4d205a5fdb1bb716c877f1a6101720945b1e
Saginoya-nm7 <sqrt.trill425@gmail.com>
2021-04-21 21:07:08 +0900

    Dear Imguiウィンドウ、ウィジェットの追加

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use gl::types::{GLfloat, GLsizei, GLsizeiptr};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+use imgui;
+use imgui_opengl_renderer;
+use imgui_sdl2;
+
 mod shader;
 mod vertex;
 
@@ -72,9 +76,30 @@ fn main() {
         VERTEX_NUM as i32,
     );
 
+    // imguiの初期化
+    // コンテキストデータの作成
+    let mut imgui_context = imgui::Context::create();
+    // 設定ファイルを作成しないようにする
+    imgui_context.set_ini_filename(None);
+
+    // imgui-sdl2の初期化
+    // SDL2で作成したウィンドウでImguiを使えるようにする
+    // imgui-sdl2のコンテキストデータを作成
+    let mut imgui_sdl2_context = imgui_sdl2::ImguiSdl2::new(&mut imgui_context, &window);
+    // 描画処理のためのレンダラーを作成
+    let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui_context, |s| {
+        video_subsystem.gl_get_proc_address(s) as _
+    });
+
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
+            // イベントがimguiによるものである場合、イベントをimgui側に渡し、無視する
+            imgui_sdl2_context.handle_event(&mut imgui_context, &event);
+            if imgui_sdl2_context.ignore_event(&event) {
+                continue;
+            }
+
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
@@ -134,11 +159,53 @@ fn main() {
             shader.set_mat4(c_str!("uView"), &view_matrix);
             shader.set_mat4(c_str!("uProjection"), &projection_matrix);
 
+            // 処理したデータに基づいて、描画を行う
             vertex.draw();
+
+            // imguiのウィンドウを描画
+            // SDL2とimgui間で画面サイズや入力などをよしなに調整する
+            imgui_sdl2_context.prepare_frame(
+                imgui_context.io_mut(),
+                &window,
+                &event_pump.mouse_state(),
+            );
+
+            // ウィジェット生成のためのUI構造体を作成
+            let ui = imgui_context.frame();
+            // imguiウィンドウ作成
+            imgui::Window::new(imgui::im_str!("Information"))
+                .size([300.0, 200.0], imgui::Condition::FirstUseEver)
+                // .build() 第2引数にウィジェット追加のコードを書く
+                .build(&ui, || {
+                    ui.text(imgui::im_str!("Hello, World!"));
+                    let mouse_pos = ui.io().mouse_pos;
+                    ui.text(format!(
+                        "Mouse Position: ({:.1}, {:.1})",
+                        mouse_pos[0], mouse_pos[1]
+                    ));
+                    imgui::ProgressBar::new(0.6)
+                        .size([200.0, 20.0])
+                        .overlay_text(imgui::im_str!("Progress!"))
+                        .build(&ui);
+                    let arr = [0.6f32, 0.1f32, 1.0f32, 0.5f32, 0.92f32, 0.1f32, 0.2f32];
+
+                    ui.plot_lines(imgui::im_str!("lines"), &arr)
+                        .graph_size([200.0, 40.0])
+                        .build();
+                    ui.plot_histogram(imgui::im_str!("lines"), &arr)
+                        .graph_size([200.0, 40.0])
+                        .build();
+                });
+
+            // imguiウィンドウの描画
+            // SDL2とimgui間で画面サイズや入力などをよしなに調整する
+            imgui_sdl2_context.prepare_render(&ui, &window);
+            // 描画
+            renderer.render(ui);
 
             window.gl_swap_window();
         }
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 144));
     }
 }
